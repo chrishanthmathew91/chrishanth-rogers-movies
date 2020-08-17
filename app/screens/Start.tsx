44067b2ae05f157d746a269b002a78be083ca321
1583732 <chrishanth.r@tcs.com>
2020-08-17 20:47:39 +0530

    Bug fixes

--- app/screens/Start.tsx
+++ app/screens/Start.tsx
@@ -1,6 +1,6 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, FlatList} from 'react-native';
 import {useValue} from 'react-native-redash';
 
 import Modal from '@components/Modal';
@@ -28,6 +28,8 @@ const Start = () => {
     const activeMovieId = useValue<number>(-1);
     const [modal, setModal] = useState<ModalState | null>(null);
 
+    console.log(JSON.stringify(movies));
+
     const open = (index: number, movie: MovieType, position: PositionType) => {
         activeMovieId.setValue(index);
         setModal({movie, position});
@@ -38,21 +40,30 @@ const Start = () => {
         setModal(null);
     };
 
+    const renderItem = ({item, index}) => {
+        return (
+            <Movie
+                activeMovieId={activeMovieId}
+                key={item.name}
+                index={index}
+                movie={item}
+                open={open}
+            />
+        );
+    };
+
+    const extractKey = (item) => item.id;
+
     return (
         <>
             <StatusBar barStyle="dark-content" />
             <SafeAreaView>
-                <ScrollView contentInsetAdjustmentBehavior="automatic">
-                    {movies.map((movie, index) => (
-                        <Movie
-                            activeMovieId={activeMovieId}
-                            key={movie.name}
-                            index={index}
-                            movie={movie}
-                            open={open}
-                        />
-                    ))}
-                </ScrollView>
+                <FlatList
+                    data={movies}
+                    keyExtractor={extractKey}
+                    renderItem={renderItem}
+                    initialNumToRender={2}
+                />
                 {modal !== null && <Modal {...modal} close={close} />}
             </SafeAreaView>
         </>
